a838ff1bad0233db5ae84ca539ed32be88dcfe1b
André Laranjeira <andrecaldaslaranjeira@gmail.com>
2019-05-07 14:57:47 -0300

    Classifier initialization * Added a section for initializing the random forest classifiers, with a default example. * Added a seed for data splitting to enable controlled testing. * Added general information prints to inform the user of the values of control variables. * Small spacing changes.

--- src/main.py
+++ src/main.py
@@ -12,10 +12,16 @@ from sklearn.metrics import confusion_matrix
 # Main function:
 
 # Control variables:
-test_size = 0.2
+random_seed = 193777
+test_size = 0.15
+
+print("General info:")
+print(">> Test size: %.2f%%" % (test_size*100))
+print(">> Seed used: %d" % (random_seed))
+print("")
 
 # Data extraction:
-leaf_dataset = pandas.read_csv('data/leaf.csv', header=None)
+leaf_dataset = pandas.read_csv('data/leaf.csv', header = None)
 leaf_features = leaf_dataset.values[:,2:]
 leaf_labels = list(map(int, leaf_dataset.values[:,0]))
 
@@ -23,5 +29,16 @@ leaf_labels = list(map(int, leaf_dataset.values[:,0]))
 train_features, test_features, train_labels, test_labels = \
 model_selection.train_test_split(leaf_features,
                                  leaf_labels,
-                                 test_size=test_size)
+                                 test_size = test_size,
+                                 random_state = random_seed)
 
+# Initializing classifiers:
+classifiers = []
+classifiers.append(('Default RF',
+                    RandomForestClassifier(n_estimators = 100,
+                                           criterion = 'gini',
+                                           max_depth = None,
+                                           min_samples_split = 2,
+                                           min_samples_leaf = 1,
+                                           max_features = 'auto',
+                                           max_leaf_nodes = None)))
