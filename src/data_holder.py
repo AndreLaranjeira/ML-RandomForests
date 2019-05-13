807775c9207a37a5edcae44cd1e13c5807295cd2
victor <victoragcosta@hotmail.com>
2019-05-13 11:19:51 -0300

    Gerados gráficos de parâmetros

---
+++ src/data_holder.py
@@ -0,0 +1,73 @@
+import numpy as np
+import pandas
+import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+
+# Por Victor vvvv
+class DataHolder:
+  def __init__(self, criterion, max_features, data):
+    self.criterion = criterion
+    self.max_features = max_features
+    self.data = data
+
+  # Por André vvvv
+  def generate_data(self, name, classifiers, train_features, train_labels, test_features, test_labels, verbose=False, random_seed=193777, scoring='accuracy', test_size=0.15):
+    acc_graph = []
+    cv_graph = []
+    for param, rf in classifiers:
+      if verbose: print("Classifier: %s" % (self.get_legend(name+' at '+str(param))))
+
+      # 10 fold cross-validation estimate:
+      kfold = model_selection.KFold(n_splits = 10, random_state = random_seed)
+      cv_results = model_selection.cross_val_score(rf, train_features,
+                                                    train_labels, cv = kfold,
+                                                    scoring = scoring)
+      if verbose: print(">> Cross-validation score: %f (%f)" %
+            (cv_results.mean(), cv_results.std()))
+
+      # Test results:
+      rf.fit(train_features, train_labels)
+      predictions = rf.predict(test_features)
+      acc = accuracy_score(test_labels, predictions)
+      if verbose: print(">> Accuracy: %f\n" % (acc))
+      # print("Classification report:")
+      # print(classification_report(test_labels, predictions))
+      # print("Confusion matrix:")
+      # print(confusion_matrix(test_labels, predictions), '\n')
+      acc_graph.append(acc)
+      cv_graph.append(cv_results)
+    return (acc_graph, cv_graph)
+  # Por André ^^^^
+
+  def generate_all_data(self, train_features, train_labels, test_features, test_labels, verbose=False):
+    data = {}
+    for name, classifiers in self.data.items():
+      data[name] = self.generate_data(name, classifiers, train_features, train_labels, test_features, test_labels, verbose=verbose)
+    self.all_data = data
+    return data
+
+  def get_max_acc(self):
+    while self.all_data == None:
+      self.generate_all_data()
+    self.max_acc = 0.0
+    self.best_classifier = None
+    for k, data in self.all_data.items():
+      for i, acc in enumerate(data[0]):
+        if acc > self.max_acc:
+          self.max_acc = acc
+          self.best_classifier = self.data[k][i][1]
+    return (self.max_acc,self.best_classifier)
+
+  def get_legend(self, end=''):
+    return self.criterion + ', ' + self.max_features + ':' + end
+
+  def plot_data(self, ax, name):
+    if self.all_data == None:
+      self.generate_all_data()
+    x = [tup[0] for tup in self.data[name]]
+    ax.plot(x, self.all_data[name][0], label=self.get_legend())
+# Por Victor ^^^^
--- src/main.py
+++ src/main.py
@@ -3,13 +3,21 @@
 # Package imports:
 import numpy as np
 import pandas
+import time
+import matplotlib.pyplot as plt
 from sklearn import model_selection
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 
+# User imports:
+from data_holder import *
+
 # Main function:
+
+# Por André vvvv
+start_time = time.time()
 
 # Control variables:
 random_seed = 193777
@@ -33,43 +41,110 @@ model_selection.train_test_split(leaf_features,
                                  leaf_labels,
                                  test_size = test_size,
                                  random_state = random_seed)
+# Por André ^^^^
 
 # Initializing classifiers:
-classifiers = []
-classifiers.append(('Default RF',
-                    RandomForestClassifier(n_estimators = 100,
-                                           criterion = 'gini',
-                                           max_depth = None,
-                                           min_samples_split = 2,
-                                           min_samples_leaf = 1,
-                                           max_features = 'auto',
-                                           max_leaf_nodes = None)))
-classifiers.append(('Entropy-500 RF',
-                    RandomForestClassifier(n_estimators = 500,
-                                           criterion = 'entropy',
-                                           max_depth = None,
-                                           min_samples_split = 2,
-                                           min_samples_leaf = 1,
-                                           max_features = 'auto',
-                                           max_leaf_nodes = None)))
+
+# Por Victor vvvv
+datas = []
+for criterion in ['entropy', 'gini']:
+  for max_features in ['sqrt', 'log2', None]:
+    changes = {}
+
+    # Analisar como variar a quantidade de árvores afeta
+    changes['n_estimators'] = []
+    for i in range(1,502,100):
+      changes['n_estimators'].append((i,
+                                      RandomForestClassifier(n_estimators = i,
+                                                            criterion = criterion,
+                                                            max_depth = None,
+                                                            min_samples_split = 2,
+                                                            min_samples_leaf = 1,
+                                                            max_features = max_features,
+                                                            max_leaf_nodes = None)))
+
+    # Analisar poda de árvore limitando profundidade máxima (espero pouco disso)
+    changes['max_depth'] = []
+    for i in range(0,14,2):
+      changes['max_depth'].append((i+1,
+                                   RandomForestClassifier(n_estimators = 100,
+                                                            criterion = criterion,
+                                                            max_depth = i+1,
+                                                            min_samples_split = 2,
+                                                            min_samples_leaf = 1,
+                                                            max_features = max_features,
+                                                            max_leaf_nodes = None)))
+
+    # Analisar poda de árvore limitando mínimo de dados para ocorrer divisão
+    changes['min_samples_split'] = []
+    for i in range(0,14,1):
+      changes['min_samples_split'].append((i+2,
+                                            RandomForestClassifier(n_estimators = 100,
+                                                            criterion = criterion,
+                                                            max_depth = None,
+                                                            min_samples_split = i+2,
+                                                            min_samples_leaf = 1,
+                                                            max_features = max_features,
+                                                            max_leaf_nodes = None)))
+
+    # Analisar poda de árvore limitando mínimo de dados por folha (slides)
+    changes['min_samples_leaf'] = []
+    for i in range(20):
+      changes['min_samples_leaf'].append((i+1,
+                                          RandomForestClassifier(n_estimators = 100,
+                                                            criterion = criterion,
+                                                            max_depth = None,
+                                                            min_samples_split = 2,
+                                                            min_samples_leaf = i+1,
+                                                            max_features = max_features,
+                                                            max_leaf_nodes = None)))
+
+    # Analisar poda limitando número de folhas
+    changes['max_leaf_nodes'] = []
+    for i in range(14, 141, 14):
+      changes['max_leaf_nodes'].append((i,
+                                        RandomForestClassifier(n_estimators = 100,
+                                                            criterion = criterion,
+                                                            max_depth = None,
+                                                            min_samples_split = 2,
+                                                            min_samples_leaf = 1,
+                                                            max_features = max_features,
+                                                            max_leaf_nodes = i)))
+    datas.append(
+      DataHolder(criterion, 'all' if max_features==None else max_features, changes)
+      )
 
 # Cross-validation predictions and test results:
-for name, rf in classifiers:
-    print("Classifier: %s" % (name))
+names = [
+  'n_estimators',
+  'max_depth',
+  'min_samples_split',
+  'min_samples_leaf',
+  'max_leaf_nodes'
+  ]
 
-    # 10 fold cross-validation estimate:
-    kfold = model_selection.KFold(n_splits = 10, random_state = random_seed)
-    cv_results = model_selection.cross_val_score(rf, train_features,
-                                                 train_labels, cv = kfold,
-                                                 scoring = scoring)
-    print(">> Cross-validation score: %f (%f)" %
-          (cv_results.mean(), cv_results.std()))
+for data in datas:
+  data.generate_all_data(train_features, train_labels, test_features, test_labels, verbose=True)
+  max_acc, best_classifier = data.get_max_acc()
+for data in datas:
+  print('Best Accuracy for ' + data.get_legend(': '+str(max_acc)) )
+  print('Parameters:')
+  for name in names:
+    print(name + ' = ' + str(getattr(best_classifier,name)))
+  print()
 
-    # Test results:
-    rf.fit(train_features, train_labels)
-    predictions = rf.predict(test_features)
-    print(">> Accuracy: %f\n" % (accuracy_score(test_labels, predictions)))
-    # print("Classification report:")
-    # print(classification_report(test_labels, predictions))
-    # print("Confusion matrix:")
-    # print(confusion_matrix(test_labels, predictions), '\n')
+for name in names:
+  fig = plt.figure(name)
+  ax = fig.gca()
+  for data in datas:
+    data.plot_data(ax, name)
+  ax.legend(loc='best')
+  ax.set_xlabel('Value of the parameter ' + name)
+  ax.set_ylabel('Accuracy')
+  fig.savefig('img/'+name+'.png')
+
+end_time = time.time()
+print('Execution time = {0:.0f} seconds'.format(end_time-start_time))
+
+plt.show()
+# Por Victor ^^^^
